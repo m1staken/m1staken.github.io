33e09a75fb1b9d8ead819a18e9b0b5fa62292847
m1staken <67109667+m1staken@users.noreply.github.com>
2025-09-03 14:53:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function showSchedule(day) {
-            const schedules = document.querySelectorAll('.schedule');
-            schedules.forEach(schedule => {
-                schedule.style.display = 'none';
-            });
+    const schedules = document.querySelectorAll('.schedule');
+    schedules.forEach(schedule => {
+        schedule.style.display = 'none';
+    });
 
-            const selectedSchedule = document.getElementById('schedule-' + day);
-            selectedSchedule.style.display = 'block';
+    const selectedSchedule = document.getElementById('schedule-' + day);
+    selectedSchedule.style.display = 'block';
 }
